5535f01d9e690b1621f0533c1496236797fb7d95
bintangnuraaini <bintangnuraini00@gmail.com>
2024-04-28 11:02:05 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "B1_Prak1A_13120220027"
+include(":app")
+ 
